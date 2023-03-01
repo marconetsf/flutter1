2e4db0ecd76adef352d5dc713d8f5fe0d75ff69e
Marcone Tenório da Silva Filho <marconetsf@gmail.com>
2023-03-01 01:26:39 -0300

    Adding padding and round on widget

--- lib/main.dart
+++ lib/main.dart
@@ -13,175 +13,255 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       title: 'Flutter Demo',
       theme: ThemeData(
-        primarySwatch: Colors.lightGreen,
+        primarySwatch: Colors.blue,
       ),
-      home: Container(
+      home: Scaffold(
+        appBar: AppBar(
+          leading: Container(),
+          title: const Text('Tarefas'),
+        ),
+        body: ListView(
+          children: const [
+            Task('Aprender Flutter', 0,
+                'https://pbs.twimg.com/media/Eu7m692XIAEvxxP?format=png&name=large'),
+            Task('Aprender Material Design', 4,
+                'https://tswbike.com/wp-content/uploads/2020/09/108034687_626160478000800_2490880540739582681_n-e1600200953343.jpg'),
+            Task('Aprender Cupertino Design', 5,
+                'https://manhattanmentalhealthcounseling.com/wp-content/uploads/2019/06/Top-5-Scientific-Findings-on-MeditationMindfulness-881x710.jpeg'),
+            Task('Aprender OOP', 5,
+                'https://thebogotapost.com/wp-content/uploads/2017/06/636052464065850579-137719760_flyer-image-1.jpg'),
+            Task('Aprender Flutter', 4,
+                'https://i.ibb.co/tB29PZB/kako-epifania-2022-2-c-pia.jpg'),
+            Task('Aprender Material Design', 1,
+                'https://pbs.twimg.com/media/Eu7m692XIAEvxxP?format=png&name=large'),
+            Task('Aprender Cupertino Design', 3,
+                'https://manhattanmentalhealthcounseling.com/wp-content/uploads/2019/06/Top-5-Scientific-Findings-on-MeditationMindfulness-881x710.jpeg'),
+            Task('Aprender OOP', 4,
+                'https://manhattanmentalhealthcounseling.com/wp-content/uploads/2019/06/Top-5-Scientific-Findings-on-MeditationMindfulness-881x710.jpeg'),
+          ],
+        ),
+        floatingActionButton: FloatingActionButton(onPressed: () {}),
+      ),
+    );
+  }
+}
+
+class ColorFull extends StatelessWidget {
+  final Color cor1;
+  final Color cor2;
+  final Color cor3;
+
+  const ColorFull(
+    this.cor1, this.cor2, this.cor3, {
+    Key? key,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Row(
+      children: [
+        Container(
+          height: 150,
+          width: 100,
+          decoration: BoxDecoration(
+            color: cor1,
+            borderRadius: BorderRadius.circular(20),
+            border: Border.all(
+            color: Colors.black,
+            width: 3,
+          ),),
+        ),
+        Container(
+          height: 150,
+          width: 100,
+          decoration: BoxDecoration(
+            color: cor2,
+            borderRadius: BorderRadius.circular(20),
+            border: Border.all(
+              color: Colors.black,
+              width: 3,
+            ),),
+          child: Icon(Icons.people_alt_sharp)
+        ),
+        Container(
+          height: 150,
+          width: 100,
+          decoration: BoxDecoration(
+            color: cor3,
+            borderRadius: BorderRadius.circular(20),
+            border: Border.all(
+              color: Colors.black,
+              width: 3,
+            ),),
+        ),
+      ],
+    );
+  }
+}
+
+class Task extends StatefulWidget {
+  final String task;
+  final String photo;
+  final int dificuldade;
+
+  const Task(this.task, this.dificuldade, this.photo, {Key? key})
+      : super(key: key);
+
+  @override
+  State<Task> createState() => _TaskState();
+}
+
+class _TaskState extends State<Task> {
+  int nivel = 1;
+
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: const EdgeInsets.all(5.0),
+      child: Container(
         color: Colors.white,
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-          crossAxisAlignment: CrossAxisAlignment.center,
+        child: Stack(
           children: [
-            Stack(
-              alignment: AlignmentDirectional.center,
-              children: [
-                Container(
-                  color: Colors.red,
-                  width: 150,
-                  height: 150,
-                ),
-                Container(
-                  color: Colors.blue,
-                  width: 75,
-                  height: 75,
-                ),
-              ],
-            ),
-            Stack(
-              alignment: AlignmentDirectional.center,
-              children: [
-                Container(
-                  color: Colors.red,
-                  width: 150,
-                  height: 150,
-                ),
-                Container(
-                  color: Colors.blue,
-                  width: 75,
-                  height: 75,
-                ),
-              ],
-            ),
-            Row(
-              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-              crossAxisAlignment: CrossAxisAlignment.center,
-              children: [
-                Container(
-                  color: Colors.cyan,
-                  height: 50,
-                  width: 50,
-                ),
-                Container(
-                  color: Colors.pinkAccent,
-                  height: 50,
-                  width: 50,
-                ),
-                Container(
-                  color: Colors.purple,
-                  height: 50,
-                  width: 50,
-                ),
-              ],
-            ),
             Container(
-              color: Colors.amber,
-              height: 30,
-              width: 300,
-              child: const Text(
-                'Diamante Amarelo',
-                style: TextStyle(
-                  color: Colors.black,
-                  fontSize: 28,
-                ),
-                textAlign: TextAlign.center,
-              ),
+              decoration: BoxDecoration(
+                  borderRadius: BorderRadius.circular(5), color: Colors.blue),
+              height: 140,
             ),
-            ElevatedButton(
-              onPressed: () {
-                print('Você apertou o botão');
-              },
-              child: const Text(
-                'Aperte o botão', style: TextStyle(fontSize: 18,),),)
+            Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                Container(
+                  decoration: BoxDecoration(
+                    borderRadius: BorderRadius.circular(10),
+                    color: Colors.white,
+                  ),
+                  height: 100,
+                  child: Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                    children: [
+                      Container(
+                        decoration: BoxDecoration(
+                          borderRadius: BorderRadius.circular(6),
+                          color: Colors.white,
+                        ),
+                        width: 72,
+                        height: 100,
+                        child: ClipRRect(
+                          borderRadius: BorderRadius.circular(4),
+                          child: Image.network(
+                            widget.photo,
+                            fit: BoxFit.cover,
+                          ),
+                        ),
+                      ),
+                      Container(
+                          width: 200,
+                          child: Column(
+                            mainAxisAlignment: MainAxisAlignment.center,
+                            crossAxisAlignment: CrossAxisAlignment.start,
+                            children: [
+                              Text(
+                                widget.task,
+                                style: const TextStyle(
+                                  fontSize: 20,
+                                  overflow: TextOverflow.ellipsis,
+                                  fontWeight: FontWeight.bold,
+                                ),
+                              ),
+                              Row(
+                                children: [
+                                  Icon(
+                                    Icons.star,
+                                    color: widget.dificuldade >= 1
+                                        ? Colors.blue
+                                        : Colors.blue[100],
+                                    size: 15,
+                                  ),
+                                  Icon(
+                                    Icons.star,
+                                    color: widget.dificuldade >= 2
+                                        ? Colors.blue
+                                        : Colors.blue[100],
+                                    size: 15,
+                                  ),
+                                  Icon(
+                                    Icons.star,
+                                    color: widget.dificuldade >= 3
+                                        ? Colors.blue
+                                        : Colors.blue[100],
+                                    size: 15,
+                                  ),
+                                  Icon(
+                                    Icons.star,
+                                    color: widget.dificuldade >= 4
+                                        ? Colors.blue
+                                        : Colors.blue[100],
+                                    size: 15,
+                                  ),
+                                  Icon(
+                                    Icons.star,
+                                    color: widget.dificuldade >= 5
+                                        ? Colors.blue
+                                        : Colors.blue[100],
+                                    size: 15,
+                                  ),
+                                ],
+                              ),
+                            ],
+                          )),
+                      Container(
+                        height: 52,
+                        width: 52,
+                        child: ElevatedButton(
+                            onPressed: () {
+                              setState(() {
+                                nivel++;
+                              });
+                              print(nivel);
+                            },
+                            child: Column(
+                              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                              crossAxisAlignment: CrossAxisAlignment.center,
+                              children: [
+                                const Icon(Icons.arrow_drop_up),
+                                Text("Up"),
+                              ],
+                            )),
+                      )
+                    ],
+                  ),
+                ),
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: [
+                    Padding(
+                      padding: const EdgeInsets.all(12.0),
+                      child: Container(
+                        child: LinearProgressIndicator(
+                          color: Colors.white,
+                          value: (widget.dificuldade > 0)
+                              ? ((nivel / widget.dificuldade * 2) / 10)
+                              : 1,
+                        ),
+                        width: 200,
+                      ),
+                    ),
+                    Padding(
+                      padding: const EdgeInsets.all(12.0),
+                      child: Text(
+                        'Nivel $nivel',
+                        style: TextStyle(
+                            color: Colors.white, fontWeight: FontWeight.bold),
+                      ),
+                    ),
+                  ],
+                ),
+              ],
+            )
           ],
         ),
       ),
     );
   }
 }
-
-
-
-// class MyApp extends StatelessWidget {
-//   const MyApp({super.key});
-//
-//   // This widget is the root of your application.
-//   @override
-//   Widget build(BuildContext context) {
-//     return MaterialApp(
-//       title: 'Flutter Demo',
-//       theme: ThemeData(
-//         primarySwatch: Colors.lightGreen,
-//       ),
-//       home: Container(
-//         child: Column(
-//           mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-//           crossAxisAlignment: CrossAxisAlignment.start,
-//           children: [
-//             Row(
-//               mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-//               crossAxisAlignment: CrossAxisAlignment.start,
-//               children: [
-//                 Container(
-//                   color: Colors.red,
-//                   height: 100,
-//                   width: 100,
-//                 ),
-//                 Container(
-//                   color: Colors.orange,
-//                   height: 100,
-//                   width: 100,
-//                 ),
-//                 Container(
-//                   color: Colors.yellow,
-//                   height: 100,
-//                   width: 100,
-//                 )
-//               ],
-//             ),
-//             Row(
-//               mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-//               crossAxisAlignment: CrossAxisAlignment.start,
-//               children: [
-//                 Container(
-//                   color: Colors.green,
-//                   height: 100,
-//                   width: 100,
-//                 ),
-//                 Container(
-//                   color: Colors.blue,
-//                   height: 100,
-//                   width: 100,
-//                 ),
-//                 Container(
-//                   color: Colors.blueAccent,
-//                   height: 100,
-//                   width: 100,
-//                 )
-//               ],
-//             ),
-//             Row(
-//               mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-//               crossAxisAlignment: CrossAxisAlignment.start,
-//               children: [
-//                 Container(
-//                   color: Colors.purple,
-//                   height: 100,
-//                   width: 100,
-//                 ),
-//                 Container(
-//                   color: Colors.pinkAccent,
-//                   height: 100,
-//                   width: 100,
-//                 ),
-//                 Container(
-//                   color: Colors.white,
-//                   height: 100,
-//                   width: 100,
-//                 )
-//               ],
-//             ),
-//           ],
-//         ),
-//       ),
-//     );
-//   }
-// }
